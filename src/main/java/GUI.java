011dbf1dc33291903b43aaa29342f4bc0d1cebbe
sarah1410 <sarah.glatt1@gmail.com>
2022-03-10 17:07:41 +0100

    fix method filterEmpty

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -1,16 +1,31 @@
+import Entity.Entity;
 import Entity.tags;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GUI {
     final String JA = "j";
     final String NEIN = "n";
 
-    public String[] getSearchTags() throws IOException {
+    public void showIdea(String idea) {
+        System.out.println("Sie können hierzu kreativ werden : ");
+    }
+
+    public String[] getSearchOpportiunities(String kriterium, List<Entity> allOptions) throws IOException {
+        if(this.wantSearch(kriterium)) {
+            return this.getFilters(kriterium, allOptions);
+        } else {
+            return new String[0];
+        }
+    }
+
+    public String[] getSearchTags(List<String> allTags) throws IOException {
         if(this.wantSearch("Tags")) {
-            return this.getTags();
+            return this.getTags(allTags);
         } else {
             return new String[0];
         }
@@ -23,18 +38,35 @@ public class GUI {
         return tastatur.readLine().equals(this.JA);
     }
 
-    private String[] getTags() throws IOException{
-        this.showOpportunities("Tags");
+    private String[] getFilters(String kriterium, List<Entity> allOptions) throws IOException{
+        this.showOpportunities(kriterium, getAllOptionsString(allOptions));
+        System.out.println("Geben Sie die gewünschten " + kriterium + " ein: ");
+
+        BufferedReader tastatur = new BufferedReader(new InputStreamReader(System.in));
+        return tastatur.readLine().split(" ");
+    }
+
+    private List<String> getAllOptionsString(List<Entity> allOptions) {
+        List<String> allOptionsString = new ArrayList<>();
+        for(Entity option : allOptions) {
+            allOptionsString.add(option.toString());
+        }
+
+        return allOptionsString;
+    }
+
+    private String[] getTags(List<String> allTags) throws IOException{
+        this.showOpportunities("Tags", allTags);
         System.out.println("Geben Sie die gewünschten Tags ein: ");
 
         BufferedReader tastatur = new BufferedReader(new InputStreamReader(System.in));
         return tastatur.readLine().split(" ");
     }
 
-    private void showOpportunities(String kriterium) {
+    private void showOpportunities(String kriterium, List<String> allOptions) {
         System.out.println("Diese " + kriterium + " können gesetzt werden:");
-        for(tags tag : tags.values()) {
-            System.out.print(tag + " ");
+        for(String option : allOptions) {
+            System.out.print(option + " ");
         }
         System.out.println();
     }
--- src/main/java/Steuerung.java
+++ src/main/java/Steuerung.java
@@ -1,25 +1,114 @@
 import jobs.YmlReader;
 import Entity.Entity;
 import Entity.entityStatus;
+import Entity.tags;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Steuerung {
 
     public static void main(String[] args) throws IOException {
         GUI gui = new GUI();
+        getAllTags();
+        getOpportunities();
 
-        gui.getSearchTags();
+        while(true) {
+            userIneraction(gui);
+        }
     }
 
-    private List<Entity> artList;
-    private List<Entity> stimmungList;
-    private List<Entity> objektList;
+    private static List<Entity> artList;
+    private static List<Entity> stimmungList;
+    private static List<Entity> objektList;
+    private static List<String> tagList = new ArrayList<>();
 
-    private void readEntitys() {
-        this.artList = YmlReader.readEntity(entityStatus.ART);
-        this.stimmungList = YmlReader.readEntity(entityStatus.STIMMUNG);
-        this.objektList = YmlReader.readEntity(entityStatus.OBJEKT);
+    private static String[] artFilter;
+    private static String[] stimmungFilter;
+    private static String[] objektFilter;
+    private static String[] tagFilter;
+
+    private static void getAllTags() {
+        for(tags tag : tags.values()) {
+            tagList.add(tag.toString());
+        }
     }
+
+    private static void userIneraction(GUI gui) throws IOException {
+        getAllSearchOpportiunities(gui);
+        gui.showIdea(getIdea());
+    }
+
+    private static void getAllSearchOpportiunities(GUI gui) throws IOException {
+        artFilter = gui.getSearchOpportiunities("Arten", artList);
+        stimmungFilter = gui.getSearchOpportiunities("Stimmungen", stimmungList);
+        objektFilter = gui.getSearchOpportiunities("Objekte", objektList);
+        tagFilter = gui.getSearchTags(tagList);
+
+        fillEmptyFilter();
+    }
+
+    private static void getOpportunities() {
+        readEntitys();
+    }
+
+    private static void readEntitys() {
+        artList = YmlReader.readEntity(entityStatus.ART);
+        stimmungList = YmlReader.readEntity(entityStatus.STIMMUNG);
+        objektList = YmlReader.readEntity(entityStatus.OBJEKT);
+    }
+
+    private static void filterObjekteIfTag() {
+        if(!filterEmpty(tagFilter)) {
+            filterObjekte();
+        }
+    }
+
+    private static void filterObjekte() {
+        //nur Objekte mit Tags
+    }
+
+    private static String getIdea() {
+        filterObjekteIfTag();
+        return randomObtions();
+    }
+
+    private static String randomObtions() {
+        String idea;
+        idea = randomFilterObtion(artFilter);
+        idea = idea.concat(" " + randomFilterObtion(stimmungFilter));
+        idea  = idea.concat(" " + randomFilterObtion(objektFilter));
+
+        return idea;
+    }
+
+    private static String randomFilterObtion(String[] filter) {
+        return filter[(int)(Math.random()*filter.length)];
+    }
+
+    private static void fillEmptyFilter() {
+        if(filterEmpty(artFilter)) {
+            artFilter = entittyListToStringArray(artList);
+        }
+        if(filterEmpty(stimmungFilter)) {
+            stimmungFilter = entittyListToStringArray(stimmungList);
+        }
+        if(filterEmpty(objektFilter)) {
+            objektFilter = entittyListToStringArray(objektList);
+        }
+    }
+
+    private static boolean filterEmpty(String[] filter) {
+        return filter.length == 0;
+    }
+
+    private static String[] entittyListToStringArray(List<Entity> entityList) {
+        String[] stringArray = new String[entityList.size()];
+        for(int entityListSizeCounter = 0; entityListSizeCounter < entityList.size(); entityListSizeCounter++) {
+            stringArray[entityListSizeCounter] = entityList.get(entityListSizeCounter).toString();
+        }
+        return stringArray;
+    }
+
 }
