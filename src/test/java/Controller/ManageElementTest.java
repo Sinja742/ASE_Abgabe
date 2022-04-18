ca2a7d06ecd247fafd68799fbb89411e80b05634
sarah1410 <sarah.glatt1@gmail.com>
2022-04-18 09:55:27 +0200

    finish ManageElementTest - still buggy in addElementFillList()

--- src/test/java/Controller/ManageElementTest.java
+++ src/test/java/Controller/ManageElementTest.java
@@ -5,12 +5,14 @@ import Entity.CategoryStatus;
 import Entity.SimpleCategory;
 import jobs.EntityBuilder;
 import jobs.TxtHandling;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -59,26 +61,176 @@ class ManageElementTest {
     }
 
     @Test
-    void deleteElement() {
+    void deleteSearchElementEmptyList() {
+        //Given
+        ManageElement manageElement = new ManageElement(this.entityBuilder);
+        String description = "element";
+        List<Category> elements = new ArrayList<>();
+
+        //When
+        List<Category> category = manageElement.deleteSearchElement(description, elements);
+
+        //Then
+        assertEquals(0, category.size());
     }
 
     @Test
-    void deleteSearchElement() {
+    void deleteSearchElementEmptyString() {
+        //Given
+        ManageElement manageElement = new ManageElement(this.entityBuilder);
+        String description = null;
+        List<Category> elements = new ArrayList<>();
+        elements.add(new SimpleCategory("element"));
+
+        //When
+        List<Category> category = manageElement.deleteSearchElement(description, elements);
+
+        //Then
+        assertEquals(elements, category);
     }
 
     @Test
-    void getCategoryToDescription() {
+    void deleteSearchElementSuccess() {
+        //Given
+        ManageElement manageElement = new ManageElement(this.entityBuilder);
+        String description = "element";
+        List<Category> elements = new ArrayList<>();
+        elements.add(new SimpleCategory("element"));
+
+        //When
+        List<Category> category = manageElement.deleteSearchElement(description, elements);
+
+        //Then
+        assertEquals(0, category.size());
     }
 
     @Test
-    void searchCategoryToDescription() {
+    void deleteSearchElementFailure() {
+        //Given
+        ManageElement manageElement = new ManageElement(this.entityBuilder);
+        String description = "element";
+        List<Category> elements = new ArrayList<>();
+        elements.add(new SimpleCategory("element1"));
+
+        //When
+        List<Category> category = manageElement.deleteSearchElement(description, elements);
+
+        //Then
+        assertEquals(elements, category);
     }
 
     @Test
-    void toArray() {
+    void searchCategoryToDescriptionEmptyList() {
+        //Given
+        ManageElement manageElement = new ManageElement(this.entityBuilder);
+        String description = "element";
+        List<Category> elements = new ArrayList<>();
+
+        //When
+        Category category = manageElement.searchCategoryToDescription(description, elements);
+
+        //Then
+        assertNull(category);
     }
 
     @Test
-    void toStringList() {
+    void searchCategoryToDescriptionEmptyString() {
+        //Given
+        ManageElement manageElement = new ManageElement(this.entityBuilder);
+        String description = null;
+        List<Category> elements = new ArrayList<>();
+        elements.add(new SimpleCategory("element"));
+
+        //When
+        Category category = manageElement.searchCategoryToDescription(description, elements);
+
+        //Then
+        assertNull(category);
+    }
+
+    @Test
+    void searchCategoryToDescriptionSuccess() {
+        //Given
+        ManageElement manageElement = new ManageElement(this.entityBuilder);
+        String description = "element";
+        List<Category> elements = new ArrayList<>();
+        elements.add(new SimpleCategory("element"));
+
+        //When
+        Category category = manageElement.searchCategoryToDescription(description, elements);
+
+        //Then
+        assertEquals(description, category.getDescription());
+    }
+
+    @Test
+    void searchCategoryToDescriptionFailure() {
+        //Given
+        ManageElement manageElement = new ManageElement(this.entityBuilder);
+        String description = "element";
+        List<Category> elements = new ArrayList<>();
+        elements.add(new SimpleCategory("element1"));
+
+        //When
+        Category category = manageElement.searchCategoryToDescription(description, elements);
+
+        //Then
+        assertNull(category);
+    }
+
+    @Test
+    void toArrayEmpty() {
+        //Given
+        List<Category> list = new ArrayList<>();
+
+        //When
+        Category[] array = ManageElement.toArray(list);
+
+        //Then
+        assertEquals(0, array.length);
+    }
+
+    @Test
+    void toArrayFill() {
+        //Given
+        List<Category> list = new ArrayList<>();
+        list.add(new SimpleCategory("element1"));
+        list.add(new SimpleCategory("element2"));
+
+        //When
+        Category[] array = ManageElement.toArray(list);
+
+        //Then
+        assertEquals(2, array.length);
+        assertEquals("element1", array[0].getDescription());
+        assertEquals("element2", array[1].getDescription());
+    }
+
+    @Test
+    void toStringListEmpty() {
+        //Given
+        List<Category> categoryList = new ArrayList<>();
+
+        //When
+        List<String> stringList = ManageElement.toStringList(categoryList);
+
+        //Then
+        assertEquals(0, stringList.size());
+    }
+
+    @Test
+    void toStringListFill() {
+        //Given
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new SimpleCategory("element1"));
+        categoryList.add(new SimpleCategory("element2"));
+
+        //When
+        List<String> stringList = ManageElement.toStringList(categoryList);
+
+        //Then
+        assertEquals(2, stringList.size());
+        assertEquals("element1", stringList.get(0));
+        assertEquals("element2", stringList.get(1));
     }
 }
